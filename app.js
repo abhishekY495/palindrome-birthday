e257ccf3939778b94416791e81a13faace89611c
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-06-07 17:04:35 +0530

    Added previous-date Palindrome and gifs

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const dateOfBirth = document.querySelector('#date-of-birth');
 const checkBtn = document.querySelector('.check-btn');
 const outputMessage = document.querySelector('#output-message');
+const gif = document.querySelector('.gif');
+
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+outputMessage.style.display = 'none';
+gif.style.display = 'none';
 
 checkBtn.addEventListener('click', clickHandler);
 
@@ -81,8 +87,6 @@ function getNextDate(date) {
     let month = date.month;
     let year = date.year;
 
-    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
     if (month === 2) {
         if (isLeapYear(year)) {
             if (day > 29) {
@@ -96,7 +100,7 @@ function getNextDate(date) {
             }
         }
     } else {
-        if (day >daysInMonth[month-1]) {
+        if (day > daysInMonth[month-1]) {
             day = 1;
             month++;
         }
@@ -115,6 +119,42 @@ function getNextDate(date) {
 
 }
 
+function getPreviousDate(date) {
+    let day = date.day - 1;
+    let month = date.month;
+    let year = date.year;
+
+    if (month === 3) {
+        if (isLeapYear(year)) {
+            if (day < 1) {
+                day = 29;
+                month--;
+            }
+        } else {
+            if (day < 1) {
+                day = 28;
+                month--;
+            }
+        }
+    } else {
+        if (month === 1 && day < 1) {
+            day = 31;
+            month = 12;
+            year--;
+        }
+        if (month > 1 && day < 1) {
+            day = daysInMonth[month - 2];
+            month--;
+        }
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year
+    }
+}
+
 function getNextPalindromeDate(date) {
     let ctr = 0;
     let nextDate = getNextDate(date);
@@ -130,6 +170,21 @@ function getNextPalindromeDate(date) {
     return [ctr, nextDate];
 }
 
+function getPreviousPalindromeDate(date) {
+    let ctr = 0;
+    let previousDate = getPreviousDate(date);
+
+    while(1) {
+        ctr++;
+        let isPalindrome = checkPalindromeForAllDateFormats(previousDate);
+        if (isPalindrome) {
+            break;
+        }
+        previousDate = getPreviousDate(previousDate);
+    }
+    return [ctr, previousDate];
+}
+
 function clickHandler() {
     let dob = dateOfBirth.value;
 
@@ -143,15 +198,29 @@ function clickHandler() {
         }
     
         let checkIsPalindrome = checkPalindromeForAllDateFormats(date);
-    
-        if (checkIsPalindrome) {
-            outputMessage.innerHTML = `Yay! your birthday is a Palindrome 🤘`
-        } else {
-            let [ctr, nextDate] = getNextPalindromeDate(date);
-            outputMessage.innerHTML = `The nearest palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year}, you missed by ${ctr} days.`
-        }
+        
+        outputMessage.style.display = 'block';
+        outputMessage.innerText = 'Calculating';
+        gif.src = './Images/calculating.gif';
+        gif.alt = 'A kid calculating meme';
+        gif.style.display = 'block';
+
+        setTimeout (() => {
+            if (checkIsPalindrome) {
+                outputMessage.innerHTML = `Yay! your birthday is a <span>Palindrome</span> 🤘`;
+                gif.src = './Images/palindrome.gif';
+                gif.alt = 'Andy from "The Office" says "I Knew It!"';
+            } else {
+                let [ctr1, nextDate] = getNextPalindromeDate(date);
+                let [ctr2, previousDate] = getPreviousPalindromeDate(date);
+                outputMessage.innerHTML = `No! your birthday is <span>Not</span> a Palindrome.<br>The Next Palindrome date is <span>${nextDate.day}-${nextDate.month}-${nextDate.year}</span>, you missed by <span>${ctr1}</span> days.<br>
+                The Previous Palindrome date was <span>${previousDate.day}-${previousDate.month}-${previousDate.year}</span>, you missed by <span>${ctr2}</span> days.`;
+                gif.src = './Images/not-palindrome.webp';
+                gif.alt = 'Jim from "The Office" mouthing "So close"';
+            }
+        }, 2500);
+    } else {
+        outputMessage.style.display = 'block';
+        outputMessage.innerHTML = `Select Date.`;
     }
 }
-
-
-const asd = {day: 12, month:7, year:1998}
